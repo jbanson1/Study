915c80a6d55b80c4df447ccebfde7414d1244ed6
Joseph Banson <33466255+jbanson1@users.noreply.github.com>
2024-05-25 14:23:20 +0100

    more

--- Python/functionBaby.py
+++ Python/functionBaby.py
@@ -4,3 +4,11 @@ def Steak () :
     print("This geezer loves his steak well done") 
 
 Steak()
+
+x = input("What is your fav book ")
+
+def Book(x):
+    print(f"Your favourite book is {x}")
+
+# x below is an example of setting default value for argument when function is called
+Book(x  = "Python is dope")
